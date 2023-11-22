f34e5f87510e889ea2e5f81583d2b9c98c08cb18
Tyuiu.IlyinME@outlook.com <maxchance05@mail.ru>
2023-11-22 15:28:19 +0500

    Task5

---
+++ Tyuiu.IlyinME.Sprint5.Task5.V24/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.IlyinME.Sprint5.Task5.V24.Lib;
+using System.IO;
+
+namespace Tyuiu.IlyinME.Sprint5.Task5.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Ильин М. Е. | АСОиУБ-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение набора данных из текстового файла                          *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнил: Ильин Максим Евгеньевич | АСОиУБ-23-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:/DataSprint5/InPutDataFileTask4V0.txt (файл взять из архива  *");
+            Console.WriteLine("* согласно вашему варианту. Создать папку в ручную С:/DataSprint5 и       *");
+            Console.WriteLine("* скопировать в неё файл) в котором есть набор значений.                  *");
+            Console.WriteLine("* Найти сумму всех двузначных целых чисел в файле.  Полученный результат  *");
+            Console.WriteLine("* вывести на консоль. У вещественных значений округлить до трёх           *");
+            Console.WriteLine("* знаков после запятой.                                                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask5V24.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine("Сумма всех двузначных целых чисел в файле: " + res);
+            Console.ReadKey();
+        }
+    }
+}
